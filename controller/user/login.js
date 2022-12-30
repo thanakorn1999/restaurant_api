5ff39a234e1d513d50aea3b1372cf9794b5a68f9
jame windows10 <thanakornn.ch@gmail.com>
2022-12-31 05:56:42 +0700

    add generateItems

--- controller/user/login.js
+++ controller/user/login.js
@@ -13,3 +13,55 @@ exports.testGet = (req, res, next) => {
   });
   // });
 };
+
+exports.generate = (req, res, next) => {
+  // [
+  //   {
+  // id: 1,
+  // title: "GROUP 1",
+  // detail: "detail GROUP 1",
+  // listMenu: [
+  //       {
+  //         id: 1,
+  //         title: "FOOD 11",
+  //         detail: "detail FOOD 11",
+  //       },
+  //     ],
+  //   },
+  // ];
+  async function generateItems(count, creator) {
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      result.push(creator(i));
+    }
+    return result;
+  }
+
+  async function main() {
+    let results = await generateItems(50, (i) => ({
+      id: i,
+      title: `ชื่อกลุ่มอาหารที่ ${i}`,
+      detail: `รายละเอียดกลุ่มอาหารที่ ${i}`,
+      listMenu: [],
+    }));
+    let allListMenu = [];
+    for (let [index, value] of results.entries()) {
+      value.listMenu = await generateItems(50, (i) => ({
+        id: `${index} : ${i}`,
+        title: `${index} : ชื่ออาหารที่ ${i}`,
+        detail: `${index} : รายละเอียดอาหารที่ ${i}`,
+        price: i * 10,
+      }));
+      allListMenu.push(...value.listMenu);
+    }
+
+    results.unshift({
+      id: null,
+      title: `ทั้งหมด`,
+      detail: `รายการทั้งหมดที่คุณมีอยู่`,
+      listMenu: allListMenu,
+    });
+    res.json(resp(true, results, null, null));
+  }
+  main();
+};
--- route/user.js
+++ route/user.js
@@ -3,4 +3,5 @@ const err = require("../controller/public/err");
 
 module.exports = function (app) {
   app.get("/API/test", user.testGet, err);
+  app.get("/API/generate", user.generate, err);
 };
