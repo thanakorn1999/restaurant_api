5f9c4ae0be4b0d8c30584658b66fe16e2df737c9
jame windows10 <thanakornn.ch@gmail.com>
2023-02-23 03:32:24 +0700

    add path login and register with bcrypt,jwt

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,34 +1,141 @@
 const resp = require("../public/response");
+let bcrypt = require("bcrypt");
 
-exports.testGet = (req, res, next) => {
-  // auth.getUserId(req.headers.authorization, (id) => {
-  let sql = `SELECT * FROM du_user`;
+//
+const passport = require("passport");
 
-  req.getConnection((err, connection) => {
+const jwt = require("jwt-simple");
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+// ------------------- Authorization ------------------- //
+exports.requireAuth = passport.authenticate("jwt", {
+  session: false,
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  // secretOrKey: process.env.SALT,
+  secretOrKey: "asda1",
+  passReqToCallback: true,
+};
+
+const jwtAuth = new JwtStrategy(jwtOptions, function (req, payload, done) {
+  return done(null, true);
+});
+
+passport.use(jwtAuth);
+
+exports.add = (req, res, next) => {
+  let { body } = req;
+
+  body.username = body.workplace_email;
+  delete body.repassword;
+
+  let sql_check = `SELECT id FROM du_user WHERE username = '${body.username}'`;
+
+  db.query(sql_check, (err, check) => {
     if (err) return next(err);
-    connection.query(sql, (err, results) => {
-      if (err) return next(err);
-      res.json(resp(true, results[0], null, null));
-    });
+    if (check.rows.length > 0) {
+      res.json(
+        resp(
+          false,
+          null,
+          "ชื่อผู้ใช้งานนี้ถูกใช้ไปแล้ว กรุณาเปลี่ยนใหม่",
+          "This email is already registered"
+        )
+      );
+    } else {
+      bcrypt.hash(body.password, 10, function (err, hash) {
+        body.password = hash;
+
+        [keys, values] = remap(body);
+
+        let sql = `INSERT INTO du_user (${keys}) VALUES (${values}) RETURNING id;`;
+        db.query(sql, (err, new_user) => {
+          if (err) return next(err);
+          // let newUserId = new_user.rows[0].id;
+          // db.query(sql_logs, (err_logs, _) => {
+          //   if (err_logs) return next(err_logs);
+          //   res.json(resp(true, null, null, null));
+          // });
+          res.json(resp(true, null, null, null));
+        });
+      });
+    }
   });
-  // });
+};
+
+exports.login = (req, res, next) => {
+  async function searchUser(sql) {
+    return new Promise((resolve) => {
+      db.query(sql, (err, result) => {
+        if (err) return next(err);
+
+        resolve(result.rows);
+      });
+    });
+  }
+
+  async function main() {
+    var { body } = req;
+    var username = body.username.toLowerCase();
+    var password = body.password;
+
+    let sql = ` SELECT * FROM du_user WHERE lower(username) =  '${username}'`;
+
+    let foundData = await searchUser(sql);
+
+    if (foundData.length > 0) {
+      let isPasswordCorrect = false;
+      let buffer = [];
+      for (const elmUSer of foundData) {
+        const validPassword = await bcrypt.compare(password, elmUSer.password);
+
+        if (validPassword) {
+          isPasswordCorrect = true;
+        }
+        let payload = {
+          id: elmUSer.id,
+          type: elmUSer.pagage_id,
+          iat: Math.floor(Date.now() / 1000),
+        };
+
+        token = jwt.encode(payload, process.env.SALT);
+        let objUser = {
+          ...elmUSer,
+          token: token,
+        };
+
+        buffer.push(objUser);
+      }
+
+      if (isPasswordCorrect) {
+        res.json(resp(true, buffer, null, null));
+      } else {
+        res.send(
+          resp(
+            false,
+            "Authroization Fali",
+            "รหัสผ่านผิดพลาด",
+            "Password is incorrect"
+          )
+        );
+      }
+    } else {
+      res.send(
+        resp(
+          false,
+          "Authroization Fali",
+          "ชื่อผู้ใช้ หรือ รหัสผ่านผิดพลาด",
+          "Username or Password is incorrect"
+        )
+      );
+    }
+  }
+  main();
 };
 
 exports.generate = (req, res, next) => {
-  // [
-  //   {
-  // id: 1,
-  // title: "GROUP 1",
-  // detail: "detail GROUP 1",
-  // listMenu: [
-  //       {
-  //         id: 1,
-  //         title: "FOOD 11",
-  //         detail: "detail FOOD 11",
-  //       },
-  //     ],
-  //   },
-  // ];
   async function generateItems(count, creator) {
     const result = [];
     for (let i = 0; i < count; i++) {
@@ -84,4 +191,17 @@ exports.generate = (req, res, next) => {
     res.json(resp(true, results, null, null));
   }
   main();
+};
+exports.testGet = (req, res, next) => {
+  // auth.getUserId(req.headers.authorization, (id) => {
+  let sql = `SELECT * FROM du_user`;
+
+  req.getConnection((err, connection) => {
+    if (err) return next(err);
+    connection.query(sql, (err, results) => {
+      if (err) return next(err);
+      res.json(resp(true, results[0], null, null));
+    });
+  });
+  // });
 };
